34c07fba8b3057defc319e75b486808f636243c2
scapula07 <bartholomewonogwu@yahoo.com>
2024-07-23 14:08:58 +0100

    first commit

---
+++ src/app/prompt/route.ts
@@ -0,0 +1,12 @@
+import {
+    FrameRequest,
+    getFrameMessage,
+    getFrameHtmlResponse,
+  } from "@coinbase/onchainkit";
+  import { NextRequest, NextResponse } from "next/server";
+
+
+  export async function POST(req: NextRequest): Promise<Response> {
+    return new NextResponse()
+
+  }
